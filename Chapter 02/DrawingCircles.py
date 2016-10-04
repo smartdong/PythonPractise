1f8fef9d32451c1585d171b5bc4ff7e5733a1ccc
YangXudong <yangxudongsmart@gmail.com>
2016-10-04 21:24:54 +0800

    Chapter 02

---
+++ Chapter 02/DrawingCircles.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.locals import *
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((600,500))
+pygame.display.set_caption("Drawing Circles")
+
+while True:
+	for event in pygame.event.get():
+		if event.type in (QUIT, KEYDOWN):
+			sys.exit()
+	
+	screen.fill((0,0,200))
+	
+	color = 255,255,0
+	position = 300,250
+	radius = 100
+	width = 10
+	
+	pygame.draw.circle(screen, color, position, radius, width)
+	
+	pygame.display.update()
