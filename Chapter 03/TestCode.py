d05a5ecce0af5e23497debb38c228ecf5a44482a
YangXudong <yangxudongsmart@gmail.com>
2016-10-07 22:48:39 +0800

    Chapter 03

---
+++ Chapter 03/TestCode.py
@@ -0,0 +1,26 @@
+sth = 123
+print(sth)
+sth = "ABC"
+print(sth)
+
+A = 123
+B = "ABC"
+C = 456
+D = "DEF"
+print(A,B,C,D)
+
+name = "John Carpenter"
+birth = "11/11/2011"
+print(name,birth)
+
+print("String","Theory", sep='-', end=':')
+print("\n")
+
+import sys
+print(sys.copyright)
+print(sys.platform)
+print(sys.version)
+
+import datetime
+from datetime import datetime, timezone
+print(datetime.now())
