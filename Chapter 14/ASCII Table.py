dcc8bf0064db869dd66934840992793bbc0c2ae8
YangXudong <yangxudongsmart@gmail.com>
2016-11-12 10:49:09 +0800

    Chapter 14

---
+++ Chapter 14/ASCII Table.py
@@ -0,0 +1,14 @@
+cols = 8
+rows = 256//cols
+table = list("" for n in range(rows+1))
+char = 0
+
+for col in range(1,cols+1):
+    for row in range(1,rows+1):
+        table[row] += '{:3.0f}'.format(char) + ' '
+        if char not in (9,10,13): 
+            table[row] += chr(char)
+        table[row] += '\t'
+        char += 1
+for row in table:
+    print(row)
